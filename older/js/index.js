698c548d850d0fefacd00689782ef4c600f6e6d2
itallosavieira <itallo.sa.vieira@gmail.com>
2020-11-30 11:16:28 -0300

    update files

---
+++ older/js/index.js
@@ -0,0 +1,19 @@
+const cards = document.querySelectorAll('.card');
+const modalOverlay = document.querySelector('.modal-overlay');
+const closeModal = document.querySelector('.close-modal');
+
+
+for (let card of cards) {
+    card.querySelector('img').src = `https://img.youtube.com/vi/${card.id}/maxresdefault.jpg`
+
+    card.addEventListener('click', function(){
+        modalOverlay.classList.add('active');
+        modalOverlay.querySelector('iframe').src = `https://www.youtube.com/embed/${card.id}`;
+    });
+};
+
+closeModal.addEventListener('click', function(){
+    modalOverlay.classList.remove('active');
+    modalOverlay.querySelector('iframe').src = '';
+});
+
